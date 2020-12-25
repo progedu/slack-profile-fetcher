f21084e0be710f63a3e18f11d91b90a8841eeb33
Soichiro Yoshimura <soichiro_yoshimura@nnn.ac.jp>
2020-12-25 15:31:17 +0900

    スクリプト完成

---
+++ README.md
@@ -0,0 +1,20 @@
+# slack-profile-fetcher
+
+全ユーザーのカスタムプロフィール入りの JSON ファイル `user_profiles.json` を出力するスクリプト
+
+### 使い方
+
+Node.js の v12.8.1 以上をインストール。コンソールにて
+
+```
+$ npm install
+$ npm run build
+$ env SLACK_TOKEN=xoxp-999999999-999999999-999999999-999999999 node dist/index.js
+```
+
+を実行。 SLACK_TOKEN には、
+
+- users:read
+- users.profile:read
+
+のスコープが付いている必要がある。
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,68 @@
-console.log('Hello TS!');
+import * as fs from 'fs';
+// WebAPI Method について → https://api.slack.com/methods
+// Slack SDK WebAPI について → https://slack.dev/node-slack-sdk/web-api
+import { WebClient } from '@slack/web-api';
+import {
+  UsersListResponse,
+  UsersProfileGetResponse,
+} from 'seratch-slack-types/web-api';
+
+const sleep = (msec: number) =>
+  new Promise((resolve) => setTimeout(resolve, msec));
+
+(async () => {
+  try {
+    const token = process.env.SLACK_TOKEN;
+    if (!token) {
+      console.log(`[ERROR] 環境変数 SLACK_TOKEN が設定されていません。`);
+      return;
+    }
+    const web = new WebClient(token);
+    let cursor: string | undefined = '';
+    let members: any[] | undefined = [];
+    let usersListResponse: UsersListResponse;
+
+    do {
+      console.log(`[INFO] users.list APIを呼び出します。 cursor: ${cursor}`);
+      usersListResponse = (await web.users.list({
+        cursor,
+        limit: 200,
+      })) as UsersListResponse;
+      members = members?.concat(usersListResponse.members);
+      console.log(
+        `[INFO] リスト情報を取得しました。 memberCount: ${members.length}`
+      );
+      cursor = usersListResponse.response_metadata?.next_cursor;
+    } while (cursor);
+
+    if (!members) {
+      console.log(`[ERROR] membersがありませんでした。`);
+      return;
+    }
+
+    const outputFilename = 'user_profiles.json';
+    const userProfiles = [];
+    let counter = 1;
+    for (const member of members) {
+      await sleep(20);
+      console.log(
+        `[INFO] ${counter++}人目/${members.length}人中 id: ${member.id} name: ${
+          member.name
+        } を取得します。`
+      );
+
+      const usersProfileGetResponse = (await web.users.profile.get({
+        user: member.id,
+      })) as UsersProfileGetResponse;
+      userProfiles.push(usersProfileGetResponse);
+
+      fs.writeFileSync(outputFilename, JSON.stringify(userProfiles));
+    }
+    console.log(
+      `[INFO] プロフィール情報取得が終了しました。 ${outputFilename} にて確認下さい。`
+    );
+  } catch (err) {
+    console.log(`[ERROR] エラーが発生しました。 err:`);
+    console.log(err);
+  }
+})();
